a89614c681b18eb356966519a9a590f20118a7bf
che-ri <rnjs1135@gmail.com>
2021-03-19 01:23:45 +0900

    onKeyPress 이벤트구현

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 class EventPractice extends Component {
   state = {
     message: "",
+    username: "",
   };
 
   handleChange = (event) => {
-    this.setState({ message: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   handleClick = () => {
-    alert(this.state.message);
-    this.setState({ message: "" });
+    alert(this.state.username + ":" + this.state.message);
+    this.setState({ message: "", username: "" });
+  };
+
+  handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      this.handleClick();
+    }
   };
 
   render() {
@@ -20,10 +27,18 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
