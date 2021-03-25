394655a94a84e420922cc331cfafc19d1e95648c
che-ri <rnjs1135@gmail.com>
2021-03-25 11:57:51 +0900

    [라이프사이클] 에러 발생시키기

--- hello-react/src/LifeCycleSample.js
+++ hello-react/src/LifeCycleSample.js
@@ -10,11 +10,11 @@ class LifeCycleSample extends Component {
 
     constructor(props) {
         super(props);
-        console.log("constructor");
+        console.log("1. constructor");
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
-        console.log("getDerivedStateFromProps");
+        console.log("2. getDerivedStateFromProps");
         if (nextProps.color !== prevState.color) {
             return { color: nextProps.color };
         }
@@ -22,17 +22,17 @@ class LifeCycleSample extends Component {
     }
 
     componentDidMount() {
-        console.log("componentDidMount");
+        console.log("3. componentDidMount");
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        console.log("shouldComponentUpdate", nextProps, nextState);
+        console.log("4. shouldComponentUpdate", nextProps, nextState);
         //숫자의 마지막 자리가 4면 리렌더링하지 않습니다.
         return nextState.number % 10 !== 4;
     }
 
     componentWillUnmount() {
-        console.log("componentWillUnmount");
+        console.log("5. componentWillUnmount");
     }
 
     handleClick = () => {
@@ -42,7 +42,7 @@ class LifeCycleSample extends Component {
     };
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log("getSnapshotBeforeUpdate");
+        console.log("6. getSnapshotBeforeUpdate");
         if (prevProps.color !== this.props.color) {
             return this.myRef.style.color;
         }
@@ -50,14 +50,14 @@ class LifeCycleSample extends Component {
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("componentDidUpdate", prevProps, prevState);
+        console.log("7. componentDidUpdate", prevProps, prevState);
         if (snapshot) {
             console.log("업데이트되기 직전 색상:", snapshot);
         }
     }
 
     render() {
-        console.log("render");
+        console.log("8. render");
 
         const style = {
             color: this.props.color,
@@ -65,6 +65,7 @@ class LifeCycleSample extends Component {
 
         return (
             <div>
+                {this.props.missing.value}
                 <h1 style={style} ref={(ref) => (this.myRef = ref)}>
                     {this.state.number}
                 </h1>
