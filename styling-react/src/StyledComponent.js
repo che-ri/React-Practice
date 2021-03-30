955ca98e1ab63803316a838c01ddb18de6adb916
che-ri <rnjs1135@gmail.com>
2021-03-31 05:01:32 +0900

    [컴포넌트 스타일링] 스타일드 컴포넌트 적용

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,15 +1,41 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled, {css} from "styled-components";
 
-const Circle = styled.div`
-width: 5rem;
-height: 5rem;
-background: black;
-border-radius: 50%;
+const Button = styled.button`
+  background: white;
+  color: black;
+  border-radius: 4px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  font-size: 1rem;
+  font-weight: 600;
+
+/* & 문자를 사용하여 Sass처럼 자기 자신 선택 가능 */
+  &:hover {
+    background: rgba(255, 255, 255, 0.9);
+  }
+
+/* 다음 코드는 inverted 값이 true일 때 특정 스타일을 부여해 줍니다. */
+${props =>props.inverted &&css
+    `
+      background: darkblue;
+      border: 2px solid white;
+      color: white;
+      &:hover {
+        background: white;
+        color: black;
+      
+    `}
 `;
 
-function StyledComponent(){
-    return <Circle/>
-}
+const StyledComponent =() =>(
+    <div color="blue" >
+        <Button inverted={true}>안녕하세요</Button>
+        <Button>테두리만</Button>
+    </div>
+);
 
 export default StyledComponent;
