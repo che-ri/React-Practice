7d95021d2e82da5c1b480e9e2440df79723868e9
che-ri <rnjs1135@gmail.com>
2021-03-31 17:27:01 +0900

    [컴포넌트스타일링] 스타일드컴포넌트 적용 2

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,6 +1,25 @@
 import React from "react";
 import styled, {css} from "styled-components";
 
+const Box = styled.div`
+/* props로 넣어 준 값을 직접 전달해 줄 수 있습니다. */
+background: ${props=>props.color || 'blue'};
+padding:1rem;
+display:flex;
+/* 기본적으로는 가로 크기 1024px에 가운데 정렬을 하고
+    가로 크기가 작아짐에 따라 크기를 줄이고
+    768px 미만이 되면 꽉 채웁니다. */
+    width:1024px;
+    margin: 0 auto;
+    @media(max-width:1024px){
+      width: 768px;
+    }
+    @media(max-width:768px){
+      width: 100%;
+    }
+` 
+
+
 const Button = styled.button`
   background: white;
   color: black;
@@ -17,9 +36,9 @@ const Button = styled.button`
   &:hover {
     background: rgba(255, 255, 255, 0.9);
   }
-
+  
 /* 다음 코드는 inverted 값이 true일 때 특정 스타일을 부여해 줍니다. */
-${props =>props.inverted &&css
+${props =>props.inverted&&css
     `
       background: darkblue;
       border: 2px solid white;
@@ -32,10 +51,10 @@ ${props =>props.inverted &&css
 `;
 
 const StyledComponent =() =>(
-    <div color="blue" >
+    <Box color="blue" >
         <Button inverted={true}>안녕하세요</Button>
         <Button>테두리만</Button>
-    </div>
+    </Box>
 );
 
 export default StyledComponent;
