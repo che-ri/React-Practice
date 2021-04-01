378c3d04c0da3cf27325fdacd3062bce627bec9d
che-ri <rnjs1135@gmail.com>
2021-04-01 17:09:51 +0900

    fix grid styling

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -1,14 +1,14 @@
-import './App.scss';
-import Header from './components/Header';
-import Content from './components/Content';
+import "./App.scss";
+import Header from "./components/Header";
+import Content from "./components/Content";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Content/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Content />
+        </div>
+    );
 }
 
 export default App;
