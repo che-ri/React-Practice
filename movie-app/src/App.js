5f7e5cb4d2e5abf72a7334cf5d0cbe8783ab9500
che-ri <rnjs1135@gmail.com>
2021-04-05 22:27:15 +0900

    props를 사용하여, 재사용 가능한 컴포넌트 만들기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+function Food(props) {
+    console.log(props);
+    return <h1 style={{ color: `${props.color}` }}>I like {props.fav}</h1>;
+}
+
 function App() {
-    return <div className="App">Hello!</div>;
+    return (
+        <>
+            <div>Hello!</div>
+            <Food
+                fav="melon"
+                color="green"
+                name="props"
+                propsArray={[1, 2, 3, 4, true]}
+            />
+            <Food fav="blueberry" color="blue" />
+            <Food fav="strawberry" color="red" />
+            <Food fav="fineapple" color="yellow" />
+        </>
+    );
 }
 
 export default App;
