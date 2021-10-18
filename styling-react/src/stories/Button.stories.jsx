12c7ffe83e24110ef781ffa0556d558f55788faa
che-ri <rnjs1135@gmail.com>
2021-10-18 14:48:31 +0900

    스토리북에 버튼 추가해보기

--- styling-react/src/stories/Button.stories.jsx
+++ styling-react/src/stories/Button.stories.jsx
@@ -1,40 +1,53 @@
-import React from 'react';
+import React from "react";
+import { action } from "@storybook/addon-actions";
 
-import { Button } from './Button';
+import Button from "../components/Button/Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: "Example/Button",
+    component: Button,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+export const Template = (args) => (
+    <Button onClick={action("clicked")} {...args}>
+        버튼입니당
+    </Button>
+);
+
+export const Emoji = () => (
+    <Button onClick={action("clicked")}>
+        <span role="img" aria-label="so cool">
+            😀😁😂🤣
+        </span>
+    </Button>
+);
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+    primary: true,
+    label: "Button",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+    label: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+    size: "large",
+    label: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+    size: "small",
+    label: "Button",
 };
