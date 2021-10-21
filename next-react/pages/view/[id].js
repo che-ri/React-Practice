f99d47c1c07354038fcd63e5b7d5a324360d8932
che-ri <rnjs1135@gmail.com>
2021-10-22 02:25:54 +0900

    정적라우팅

---
+++ next-react/pages/view/[id].js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// url뒤에 view/ 뒤에 어떠한 값을 넣어도 라우팅 된다.
+const DynamicRouteComponent = () => {
+    return <div>hello! This Component's DynamicRouterComponent</div>;
+};
+
+export default DynamicRouteComponent;
