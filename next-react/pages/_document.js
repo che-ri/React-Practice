6f4e601f460dd1b72121edf998bbd71af8583686
che-ri <rnjs1135@gmail.com>
2021-10-25 16:39:12 +0900

    2021-10-25

---
+++ next-react/pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+//next.js에서 제공하는 document를 수정할 수 있다. html이나 head, body를 수정해야될 때는 이 파일을 필수적으로 작성해야함!
+//document는 서버에서만 렌더링되고, onClick같은 이벤트 핸들러는 작용하지 않는다.
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="ko">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
