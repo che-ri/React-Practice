eb2c14e6fd7bb21836750542f48af730a637a975
che-ri <rnjs1135@gmail.com>
2021-10-27 16:01:58 +0900

    api 연결

--- next-react/pages/index.js
+++ next-react/pages/index.js
@@ -1,4 +1,21 @@
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+
 export default function Home() {
+    const [list, setList] = useState([]);
+    const API_URL =
+        "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+    function getData() {
+        Axios.get(API_URL)
+            .then((res) => setList(res.data))
+            .catch((err) => window.alert(err));
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div>
             <title>셰리</title>
